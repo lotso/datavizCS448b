44073d5c367cdd08b8e633343423b7e8834ed34a
smittens <smittens@gmail.com>
2011-12-06 17:36:01 -0800

    bargraph.JS!

---
+++ dataviz/bargraph.js
@@ -0,0 +1,93 @@
+var chart, x, y;
+var chartWidth = 800;
+var chartHeight = 600;
+var barWidth = 25;
+function drawBarGraph(subjectName, data, labels, width, height)
+{
+    data = [24, 13, 22, 10, 5];
+    labels = ["Aasdf", "asB", "C", "D", "E"];
+    
+    if(width) chartWidth = width;
+    if(height) chartHeight = height;
+    barWidth = chartHeight/(data.length*1.3);
+    var padding = chartHeight - (barWidth * data.length);
+    padding /= (data.length*2);
+    
+    var bigView = (height > 400);
+    if(bigView)
+    {
+        padding = 0;
+        //chartHeight += 60;
+    }
+
+		
+    chart = d3.select("body")
+        .append("svg:svg")
+            .attr("class","chart")
+            .attr("width",chartWidth)
+            .attr("height",chartHeight)
+        .append("svg:g")
+            .attr("transform", "translate(10,15)");
+            
+    x = d3.scale.linear()
+        .domain([0, d3.max(data)])
+        .range([0,chartWidth - 20]);
+    xHalf = d3.scale.linear()
+        .domain([0, d3.max(data)])
+        .range([0,(chartWidth)/2]);
+    y = d3.scale.ordinal()
+        .domain(data)
+        .rangeBands([0, barWidth*data.length]);
+    
+    if(bigView)
+    {
+    chart.selectAll("line")
+        .data(x.ticks(10)).enter().append("svg:line")
+            .attr("x1", x)
+            .attr("x2", x)
+            .attr("y1", 0)
+            .attr("y2", chartHeight-38)
+            .attr("stroke", "#ccc");
+    chart.selectAll("text.rule")
+        .data(x.ticks(10)).enter().append("svg:text")
+            .attr("class", "rule")
+            .attr("x", x)
+            .attr("y", chartHeight-24)
+            .attr("dy", -3)
+            .attr("text-anchor", "middle")
+            .text(String);
+    }
+    
+    chart.selectAll("rect").data(data).enter()
+        .append("svg:rect")
+            .attr("y", function(d,i) {return i*barWidth*1.2 + padding;})
+            .attr("width",x)
+            .attr("height",y.rangeBand());
+            
+    chart.selectAll("text.bar").data(data).enter()
+        .append("svg:text")
+            .attr("class", "bar")
+            .attr("x", xHalf)
+            .attr("y", function(d, i) {return i*barWidth*1.2 + barWidth/2+padding/2; })
+            .attr("dx", -3)
+            .attr("dy", ".5em")
+            .attr("text-anchor", "middle")
+            .text(function(v)
+						        {
+						            var i = 0;
+						            for(i=0;i<data.length;i++)
+						            {
+						                if(v == data[i])
+						                    return labels[i];
+						            }
+						            return "";
+						        });
+
+    var lineHeight = chartHeight;
+    if(bigView) lineHeight -= 38;
+    chart.append("svg:line")
+        .attr("y1", 0)
+        .attr("y2", lineHeight)
+        .attr("stroke", "#000");
+}
+drawBarGraph("Sen. Facer", 0, 0, 1000, 600);
